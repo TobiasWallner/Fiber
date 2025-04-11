7e5dfeadafe80c54fdadfd49894b78f8304cd0d2
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-12 01:02:00 +0200

    changed output stream for test errors results, cout -> cerr. + added tests that expect binary operators to fail

--- embed/test.cpp
+++ embed/test.cpp
@@ -4,41 +4,47 @@ namespace embed{
     
     static constexpr const char* expected_true_but  = "    expected `true`  but: `";
     static constexpr const char* expected_false_but = "    expected `false` but: `";
+                                                      
 
     void print_failed_function_file_line(const char* signature, const char* file, std::size_t line){
-        embed::cout << '[' << embed::ansi::bright_red << embed::ansi::bold << "Failed" << embed::ansi::reset << "]: test function" << embed::newl;
-        embed::cout << "    in: " << embed::ansi::magenta << signature << embed::ansi::reset << embed::newl;
-        embed::cout << "    at: " << embed::ansi::grey << file << ':' << line << embed::ansi::reset << embed::newl;
+        embed::cerr << '[' << embed::ansi::bright_red << embed::ansi::bold << "Failed" << embed::ansi::reset << "]: test function" << embed::newl;
+        embed::cerr << "    in: " << embed::ansi::magenta << signature << embed::ansi::reset << embed::newl;
+        embed::cerr << "    at: " << embed::ansi::grey << file << ':' << line << embed::ansi::reset << embed::newl;
     }
 
     void TEST_FUNC_print_1(const char* signature, const char* file, std::size_t line, const char* test_func, const char* val_str){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << expected_true_but << test_func << '(' << val_str << ")`" << embed::newl;
+        embed::cerr << expected_true_but << test_func << '(' << val_str << ")`" << embed::newl;
     }
     
     void TEST_FUNC2_print_1(const char* signature, const char* file, std::size_t line, const char* test_func, const char* lhs_str, const char* rhs_str){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << expected_true_but << test_func << '(' << lhs_str << ", " << rhs_str << ")`" << embed::newl;
+        embed::cerr << expected_true_but << test_func << '(' << lhs_str << ", " << rhs_str << ")`" << embed::newl;
     }
     
     void TEST_BINARY_OPERATOR_print_1(const char* signature, const char* file, std::size_t line, const char* lhs_str, const char* op, const char* rhs_str){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << expected_true_but << lhs_str << ' ' << op << ' ' << rhs_str << '`' << embed::newl;
+        embed::cerr << expected_true_but << lhs_str << ' ' << op << ' ' << rhs_str << '`' << embed::newl;
+    }
+
+    void TEST_NOT_BINARY_OPERATOR_print_1(const char* signature, const char* file, std::size_t line, const char* lhs_str, const char* op, const char* rhs_str){
+        print_failed_function_file_line(signature, file, line);
+        embed::cerr << expected_false_but << lhs_str << ' ' << op << ' ' << rhs_str << '`' << embed::newl;
     }
 
     void TEST_TRUE_print_1(const char* signature, const char* file, std::size_t line, const char* val){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << expected_true_but << val << '`' << embed::newl;
+        embed::cerr << expected_true_but << val << '`' << embed::newl;
     }
 
     void TEST_FALSE_print_1(const char* signature, const char* file, std::size_t line, const char* val){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << expected_false_but << val << '`' << embed::newl;
+        embed::cerr << expected_false_but << val << '`' << embed::newl;
     }
 
     void TEST_ERROR_print(const char* signature, const char* file, std::size_t line){
         print_failed_function_file_line(signature, file, line);
-        embed::cout << embed::ansi::yellow << "    This should not have been reached" << embed::ansi::reset << embed::newl << embed::endl;
+        embed::cerr << embed::ansi::yellow << "    This should not have been reached" << embed::ansi::reset << embed::newl << embed::endl;
     }
     
 }
--- embed/test.hpp
+++ embed/test.hpp
@@ -6,40 +6,47 @@
 // embed
 #include "OStream.hpp"
 #include "definitions.hpp"
-#include "ansi_codes.hpp"
+#include "ansi.hpp"
 
 
 namespace embed{
 
-    static constexpr const char* indented_arrow     = "                              --> `";
+    static constexpr const char* indented_arrow     = "                      --> `";
 
     void print_failed_function_file_line(const char* signature, const char* file, std::size_t line);
 
     void TEST_FUNC_print_1(const char* signature, const char* file, std::size_t line, const char* test_func, const char* val_str);
     template<class T, class R>
     void TEST_FUNC_print_2(const char* test_func, const T& val, const R& result){
-        embed::cout << indented_arrow << test_func << '(' << (val) << ")`" << embed::newl;
-        embed::cout << indented_arrow << result << embed::newl << embed::endl;
+        embed::cerr << indented_arrow << test_func << '(' << (val) << ")`" << embed::newl;
+        embed::cerr << indented_arrow << result << embed::newl << embed::endl;
     }
 
     void TEST_FUNC2_print_1(const char* signature, const char* file, std::size_t line, const char* test_func, const char* lhs_str, const char* rhs_str);
     template<class T1, class T2, class R>
     void TEST_FUNC2_print_2(const char* test_func, const T1& lhs, const T2& rhs, const R& result){
-        embed::cout << indented_arrow << test_func << '(' << lhs << ", " << rhs << ")`" << embed::newl;
-        embed::cout << indented_arrow << result << '`' << embed::newl << embed::endl;
+        embed::cerr << indented_arrow << test_func << '(' << lhs << ", " << rhs << ")`" << embed::newl;
+        embed::cerr << indented_arrow << result << '`' << embed::newl << embed::endl;
     }
 
     void TEST_BINARY_OPERATOR_print_1(const char* signature, const char* file, std::size_t line, const char* lhs_str, const char* op, const char* rhs_str);
     template<class T1, class T2, class R>
     void TEST_BINARY_OPERATOR_print_2(const T1& lhs, const char* op, const T2& rhs, const R& result){
-        embed::cout << indented_arrow << lhs << ' ' << op << ' ' << rhs << '`' << embed::newl;
-        embed::cout << indented_arrow << result << '`' << embed::newl << embed::endl;
+        embed::cerr << indented_arrow << lhs << ' ' << op << ' ' << rhs << '`' << embed::newl;
+        embed::cerr << indented_arrow << result << '`' << embed::newl << embed::endl;
+    }
+
+    void TEST_NOT_BINARY_OPERATOR_print_1(const char* signature, const char* file, std::size_t line, const char* lhs_str, const char* op, const char* rhs_str);
+    template<class T1, class T2, class R>
+    void TEST_NOT_BINARY_OPERATOR_print_2(const T1& lhs, const char* op, const T2& rhs, const R& result){
+        embed::cerr << indented_arrow << lhs << ' ' << op << ' ' << rhs << '`' << embed::newl;
+        embed::cerr << indented_arrow << result << '`' << embed::newl << embed::endl;
     }
 
     void TEST_TRUE_print_1(const char* signature, const char* file, std::size_t line, const char* val);
     template<class R>
     void TEST_TRUE_print_2(const R& val){
-        embed::cout << indented_arrow << val << '`' << embed::newl << embed::endl;
+        embed::cerr << indented_arrow << val << '`' << embed::newl << embed::endl;
     }
 
     void TEST_FALSE_print_1(const char* signature, const char* file, std::size_t line, const char* val);
@@ -74,12 +81,24 @@ namespace embed{
     }\
 }
 
+#define TEST_NOT_BINARY_OPERATOR(lhs, op, rhs){\
+    if(((lhs) op (rhs))){\
+        embed::TEST_NOT_BINARY_OPERATOR_print_1(EMBED_FUNCTION_SIGNATURE, __FILE__, __LINE__, #lhs, #op, #rhs);\
+        embed::TEST_NOT_BINARY_OPERATOR_print_2((lhs), #op, (rhs), ((lhs) op (rhs)));\
+    }\
+}
+
 #define TEST_EQUAL(lhs, rhs)            TEST_BINARY_OPERATOR(lhs, ==, rhs)
 #define TEST_NOT_EQUAL(lhs, rhs)        TEST_BINARY_OPERATOR(lhs, !=, rhs)
 #define TEST_SMALLER(lhs, rhs)          TEST_BINARY_OPERATOR(lhs, <, rhs)
 #define TEST_SMALLER_EQUAL(lhs, rhs)    TEST_BINARY_OPERATOR(lhs, <=, rhs)
 #define TEST_GREATER(lhs, rhs)          TEST_BINARY_OPERATOR(lhs, >, rhs)
 #define TEST_GREATER_EQUAL(lhs, rhs)    TEST_BINARY_OPERATOR(lhs, >=, rhs)
+
+#define TEST_NOT_SMALLER(lhs, rhs)          TEST_NOT_BINARY_OPERATOR(lhs, <, rhs)
+#define TEST_NOT_SMALLER_EQUAL(lhs, rhs)    TEST_NOT_BINARY_OPERATOR(lhs, <=, rhs)
+#define TEST_NOT_GREATER(lhs, rhs)          TEST_NOT_BINARY_OPERATOR(lhs, >, rhs)
+#define TEST_NOT_GREATER_EQUAL(lhs, rhs)    TEST_NOT_BINARY_OPERATOR(lhs, >=, rhs)
 
 #define TEST_TRUE(val) {\
     if(!(val)){\
