ac267e16ba8c89c300caeffd61b914054cf85463
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-13 19:10:16 +0200

    deleted the old OS

--- embed/OS/BaseTask_test.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace embed
-{
-    void BaseTask_test(void);
-} // namespace embed
--- embed/OS/ExecutionTimes.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/// @brief The measured execution time before the first launch in a cycle and its last suspendsion
-struct ExecutionTimes{
-    std::chrono::nanoseconds start;     ///< Actual measured cycle-start time of a task
-    std::chrono::nanoseconds end;       ///< Actual measured cycle-end time of a task
-};
