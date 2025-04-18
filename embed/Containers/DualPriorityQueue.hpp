264299c9a30a136da21f39f5ccf6832104ac6c9f
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-18 19:22:41 +0200

    cleanly split into two seperate priority queues

--- embed/Containers/DualPriorityQueue.hpp
+++ embed/Containers/DualPriorityQueue.hpp
@@ -5,23 +5,8 @@
 namespace embed
 {
 
-    /**
-     * @brief a dual stage priority list.
-     * 
-     * Stage 1 is sorted using `stage1_greater_priority()`.
-     * Elements transition from stage 1 to stage 2 if they pass the `transition_condition()`
-     * Stage 2 is sorted using the `stage2_greater_priority()`.
-     * `.top()` then returns the top priority element of stage 2
-     * 
-     * Elements are emplaced into stage 1 if the `transition_condition()` fails and directly into stage 2 if it passes
-     * 
-     * @tparam T The type of the elements in the queues
-     * @tparam N The maximum number of elements for both stages combined
-     * @tparam stage1_greater_priority A priority function for the first stage that returns true if the first argument has a greater priority than the second one
-     * @tparam stage2_greater_priority A priority function for the second stage that returns true if the first argument has a greater priority than the second one
-     * @tparam transition_condition The condition to promote elements from the first stage into the second stage
-     */
-    template<class T, size_t N, class stage1_greater_priority, class stage2_greater_priority, class transition_condition>
+    
+    template<class T, size_t N, class stage1_greater_priority, class stage2_greater_priority>
     class DualPriorityQueue{
     private:
         DualArrayList<T, N> _buffer;
@@ -50,8 +35,13 @@ namespace embed
         /// @brief returns the size/count of live elements in the container 
         constexpr size_type size() const {return this->_buffer.size();}
 
+        constexpr size_type stage1_size() const {return this->stage1().size();}
+
+        constexpr size_type stage2_size() const {return this->stage2().size();}
+
         /// @brief returns the capacity of the container. Since this is a statically allocated container this is also the maximal size.
-        constexpr size_type capacity() const {return this->_buffer.max_size();}
+        constexpr size_type stage1_capacity() const {return this->stage1().capacity();}
+        constexpr size_type stage2_capacity() const {return this->stage2().capacity();}
 
         /// @brief returns the maximal number of elements that can be stored in the container 
         constexpr size_type max_size() const {return this->_buffer.max_size();}
@@ -61,71 +51,158 @@ namespace embed
 
         /// @brief returns true if there are not elements in the container, aka. the container is empty. 
         constexpr bool empty() const {return this->_buffer.empty();}
+        constexpr bool stage1_empty() const {return this->stage1().empty();}
+        constexpr bool stage2_empty() const {return this->stage2().empty();}
 
         /// @brief returns true if the container is full and no more elements can be stored in the container 
         constexpr bool full() const {return this->_buffer.full();}
 
         /// @brief returns a reference to the element with the top most priority 
-        constexpr reference top() {return this->stage2().front();}
+        constexpr reference stage1_top() {return this->stage1().front();}
 
         /// @brief returns a reference to the element with the top most priority 
-        constexpr const_reference top() const {return this->stage2().front();}
+        constexpr const_reference stage1_top() const {return this->stage1().front();}
 
-        /// @brief promotes/transitions all consecutive top elements of stage 1 that pass the transition condition into stage 2
-        void promote(){
-            while(transition_condition(this->stage1().front())){
-                // get first element from stage 1 and restore its heap
-                value_type temp = std::move(this->stage1().front());
-                std::ranges::pop_heap(this->stage1(), stage1_greater_priority);
-                this->stage1().pop_back();
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr reference stage2_top() {return this->stage2().front();}
 
-                // emplace it into stage 2 and restore its heap
-                this->stage2().emplace_back(std::move(temp));
-                std::ranges::push_heap(this->stage2(), stage2_greater_priority);
-            }
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr const_reference stage2_top() const {return this->stage2().front();}
+
+        void stage1_push(const T& value){
+            this->stage1().emplace_back(value);
+            std::ranges::push_heap(this->stage1(), stage1_greater_priority);
         }
 
-        /**
-         * @brief Adds a value into its corresponding stage in the heap
-         * 
-         * If a value fullfills the `transition_condition()`, it will be placed directly into the
-         * stage 2 heap, skipping stage 1.
-         * 
-         * If a value does not fulfull the `transition_condition()`, it will be placed into the stage 1 heap
-         * 
-         * > Note: that `.push()` does not promote any other elements from stage 1 into stage 2
-         * 
-         * @param value the value to be inserted
-         */
-        void push(const T& value){
-            if(transition_condition(value)){
-                this->stage2().emplace_back(value);
-                std::ranges::push_heap(this->stage2(), stage2_greater_priority);
-            }else{
-                this->stage1().emplace_back(value);
-                std::ranges::push_heap(this->stage1(), stage1_greater_priority);
-            }
+        void stage1_push(T&& value){
+            this->stage1().emplace_back(std::move(value));
+            std::ranges::push_heap(this->stage1(), stage1_greater_priority);
         }
 
-        /**
-         * @brief removes the top (from stage 2) and re-establishes its heap
-         * 
-         * > Note: this function does not promote any values from stage 1 to stage 2
-         */
-        void pop(){
+        void stage2_push(const T& value){
+            this->stage2().emplace_back(value);
+            std::ranges::push_heap(this->stage2(), stage2_greater_priority);
+        }
+
+        void stage2_push(T&& value){
+            this->stage2().emplace_back(std::move(value));
+            std::ranges::push_heap(this->stage2(), stage2_greater_priority);
+        }
+
+
+        void stage2_pop(){
             std::ranges::pop_heap(this->stage2(), stage2_greater_priority);
             this->stage2().pop_back();
         }
 
-        /**
-         * @brief returns the top most element (from stage 2) and removes it from the heap
-         */
-        T top_pop(){
-            T result = std::move(this->top());
-            this->pop();
+        void stage1_pop(){
+            std::ranges::pop_heap(this->stage1(), stage2_greater_priority);
+            this->stage1().pop_back();
+        }
+
+
+        T stage1_top_pop(){
+            T result = std::move(this->stage1_top());
+            this->stage1_pop();
+            return result;
+        }
+
+        T stage2_top_pop(){
+            T result = std::move(this->stage2_top());
+            this->stage2_pop();
             return result;
         }
 
     }    
+
+
+    template<class T, size_t N, class stage1_greater_priority, class stage2_greater_priority>
+    class Stage1DualPriorityQueue{
+        private:
+        DualPriorityQueue<T, N, stage1_greater_priority, stage2_greater_priority>& _queue;
+
+        public:
+        DualPriorityQueue1(DualPriorityQueue<T, N, stage1_greater_priority, stage2_greater_priority>& queue)
+            : _queue(queue){}
+
+        /// @brief returns the size/count of live elements in the container 
+        constexpr size_type size() const {return this->_queue.stage1_size();}
+
+        /// @brief returns the capacity of the container. Since this is a statically allocated container this is also the maximal size.
+        constexpr size_type capacity() const {return this->_queue.stage1_capacity();}
+
+        /// @brief returns the maximal number of elements that can be stored in the container 
+        constexpr size_type max_size() const {return this->_queue.max_size();}
+
+        /// @brief returns the reserve - number of elements that can be stored until the container is full 
+        constexpr size_type reserve() const {return this->_queue.reserve();}
+
+        /// @brief returns true if there are not elements in the container, aka. the container is empty. 
+        constexpr bool empty() const {return this->_queue.stage1_empty();}
+
+        /// @brief returns true if the container is full and no more elements can be stored in the container 
+        constexpr bool full() const {return this->_queue.full();}
+
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr reference top() {return this->_queue.stage1_top()();}
+
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr const_reference top() const {return this->_queue.stage1_top();}
+
+
+        inline void push(const T& value){return this->_queue.stage1_push(value);}
+
+        inline void push(T&& value){return this->_queue.stage1_push(std::move(value));}
+
+
+        inline void pop(){return this->_queue.stage1_pop();}
+
+        inline T top_pop(){return this->_queue.stage1_top_pop();}
+    };  
+
+
+    template<class T, size_t N, class stage1_greater_priority, class stage2_greater_priority>
+    class Stage2DualPriorityQueue{
+        private:
+        DualPriorityQueue<T, N, stage1_greater_priority, stage2_greater_priority>& _queue;
+
+        public:
+        Stage2DualPriorityQueue(DualPriorityQueue<T, N, stage1_greater_priority, stage2_greater_priority>& queue)
+            : _queue(queue){}
+
+        /// @brief returns the size/count of live elements in the container 
+        constexpr size_type size() const {return this->_queue.stage2_size();}
+
+        /// @brief returns the capacity of the container. Since this is a statically allocated container this is also the maximal size.
+        constexpr size_type capacity() const {return this->_queue.stage2_capacity();}
+
+        /// @brief returns the maximal number of elements that can be stored in the container 
+        constexpr size_type max_size() const {return this->_queue.max_size();}
+
+        /// @brief returns the reserve - number of elements that can be stored until the container is full 
+        constexpr size_type reserve() const {return this->_queue.reserve();}
+
+        /// @brief returns true if there are not elements in the container, aka. the container is empty. 
+        constexpr bool empty() const {return this->_queue.stage2_empty();}
+
+        /// @brief returns true if the container is full and no more elements can be stored in the container 
+        constexpr bool full() const {return this->_queue.full();}
+
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr reference top() {return this->_queue.stage2_top()();}
+
+        /// @brief returns a reference to the element with the top most priority 
+        constexpr const_reference top() const {return this->_queue.stage2_top();}
+
+
+        inline void push(const T& value){return this->_queue.stage2_push(value);}
+
+        inline void push(T&& value){return this->_queue.stage2_push(std::move(value));}
+
+
+        inline void pop(){return this->_queue.stage2_pop();}
+
+        inline T top_pop(){return this->_queue.stage2_top_pop();}
+    };
 } //namespace embed
 
