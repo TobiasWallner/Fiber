8b54469c08ea42b1d90fe8f8530bfad08a8daf7e
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-25 02:41:54 +0200

    added logging via templated classes and concepts

--- embed/OS/RealTimeScheduler.hpp
+++ embed/OS/RealTimeScheduler.hpp
@@ -20,69 +20,133 @@ namespace embed
         return;
     }
 
-    template<CClock Clock>
-    class ILogger{
-    public:
-        using cock = Clock;
-        using time_point = typename Clock::time_point;
-        using duration = typename Clock::duration;
-        
-        virtual inline ~ILogger(){}
+    template<class Logger>
+    concept CRealTimeSchedulerLogger = requires(
+        Logger::time_point time, 
+        Logger::time_point time_after, 
+        Logger::time_point time_until, 
+        const char* name, 
+        unsigned int id, 
+        const char* from_queue, 
+        const char* to_queue) 
+    {
+        typename Logger::time_point;
 
-        virtual inline void log_add(time_point time, const char* name, unsigned int id, const char* to_queue);
-        virtual inline void log_move(time_point time, const char* name, unsigned int id, const char* from_queue, const char* to_queue);
-        virtual inline void log_resume(time_point time_from, time_point time_to, const char* name, unsigned int id);
-        virtual inline void log_remove(time_point time, const char* name, unsigned int id);
-        virtual inline void log_sleep(time_point time, time_point sleep_until);
+        { Logger::log_add(time, name, id, to_queue) };
+        { Logger::log_move(time, name, id, from_queue, to_queue) };
+        { Logger::log_resume(time, time_after, name, id) };
+        { Logger::log_delete(time, name,id) };
+        { Logger::log_sleep(time, time_until) };
     };
 
-    template<CClock Clock, size_t N>
-    class Logger : public ILogger<Clock>{
+    template<CClock Clock>
+    class NullLogger {
     public:
         using cock = Clock;
         using time_point = typename Clock::time_point;
         using duration = typename Clock::duration;
 
-        struct TaskAdd{time_point time; const char* name; unsigned int id; const char* to_queue;};
-        struct TaskMove{time_point time; const char* name; unsigned int id; const char* from_queue; const char* to_queue;};
-        struct TaskResume{time_point time_from; time_point time_to; const char* name; unsigned int id;};
-        struct TaskRemove{time_point time; const char* name; unsigned int id;};
-        struct Sleep{time_point time; time_point sleep_until;};
+        static inline void log_add([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id, [[maybe_unused]]const char* to_queue){}
+        static inline void log_move([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id, [[maybe_unused]]const char* from_queue, [[maybe_unused]]const char* to_queue){}
+        static inline void log_resume([[maybe_unused]]time_point time_from, [[maybe_unused]]time_point time_after, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id){}
+        static inline void log_delete([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id){}
+        static inline void log_sleep([[maybe_unused]]time_point time, [[maybe_unused]]time_point sleep_until){}
 
-        using Event = typename std::variant<TaskAdd, TaskMove, TaskResume, TaskRemove, Sleep>;
-    private:
-        ArrayList<Event, N> _event_list;
+    };
 
+    template<CClock Clock>
+    class OutputLogger {
     public:
+        using cock = Clock;
+        using time_point = typename Clock::time_point;
+        using duration = typename Clock::duration;
 
-        inline void log_add(time_point time, const char* name, unsigned int id, const char* to_queue) override {
-            this->_event_list.emplace_back(TaskAdd{time, name, id, to_queue});
+        static inline OStreamRef stream;
+
+        static void print_symbol(const char* symbol){
+            stream << embed::ansi::blue << symbol;
         }
 
-        inline void log_move(time_point time, const char* name, unsigned int id, const char* from_queue, const char* to_queue) override {
-            this->_event_list.emplace_back(TaskMove{time, name, id, from_queue, to_queue});
+        static void print_time_point(time_point time){
+            stream << embed::ansi::blue << time;
         }
 
-        inline void log_resume(time_point time_from, time_point time_to, const char* name, unsigned int id) override {
-            this->_event_list.emplace_back(TaskResume{time_from, time_to, name, id});
+        static void print_time_point(duration duration){
+            stream << embed::ansi::light_blue << duration;
         }
 
-        inline void log_remove(time_point time, const char* name, unsigned int id) override {
-            this->_event_list.emplace_back(TaskRemove{time, name, id});
+        static void print_primary_action(const char* action){
+            stream << " " << embed::ansi::light_orange << action << " ";
         }
 
-        inline void log_sleep(time_point time, time_point sleep_until) override {
-            // prevent log bloat
-            // do not record the same sleep command multiple times.
-            if(this->_event_list.empty()){
-                this->_event_list.emplace_back(Sleep{time, sleep_until});
-            }else if (std::holds_alternative<Sleep>(this->_event_list.back())){
-                if(std::get<Sleep>(this->_event_list.back()).sleep_until != sleep_until){
-                    this->_event_list.emplace_back(Sleep{time, sleep_until});
-                }
-            }
+        static void print_secondary_action(const char* action){
+            stream << " " << embed::ansi::blue << action << " ";
         }
-        
+
+        static void print_name(const char* name){
+            stream << embed::ansi::light_blue << name;
+        }
+
+        static void print_id(unsigned int id){
+            stream << embed::ansi::light_yellow << id;
+        }
+
+        static void print_time(time_point time){
+            print_symbol("@");
+            print_time_point(time);
+        }
+
+        static void print_task(const char* name, unsigned int id){
+            print_symbol("{");
+            print_name(name);
+            print_symbol(", ");
+            print_id(id);
+            print_symbol("}");
+        }
+
+        static void log_add(time_point time, const char* name, unsigned int id, const char* to_queue){
+            print_time(time);
+            print_primary_action("add");
+            print_task(name, id);
+            print_secondary_action("to");
+            print_name(to_queue);
+            stream << embed::ansi::reset << embed::endl;
+        }
+
+        static void log_move(time_point time, const char* name, unsigned int id, const char* from_queue, const char* to_queue){
+            print_time(time);
+            print_primary_action("move");
+            print_task(name, id);
+            print_secondary_action("from");
+            print_name(from_queue);
+            print_secondary_action("to");
+            print_name(to_queue);
+            stream << embed::ansi::reset << embed::endl;
+        }
+
+        static void log_resume(time_point time_from, time_point time_after, const char* name, unsigned int id){
+            print_time(time_from);
+            print_primary_action("resume");
+            print_task(name, id);
+            print_secondary_action("time");
+            print_time_point(time_after - time_from);
+            stream << embed::ansi::reset << embed::endl;
+        }
+
+        static void log_delete(time_point time, const char* name, unsigned int id){
+            print_time(time);
+            print_primary_action("delete");
+            print_task(name, id);
+            stream << embed::ansi::reset << embed::endl;
+        }
+
+        static void log_sleep(time_point time, time_point sleep_until){
+            print_time(time);
+            print_primary_action("sleep until");
+            print_time_point(sleep_until);
+            stream << embed::ansi::reset << embed::endl;
+        }
+
     };
 
     /**
@@ -92,10 +156,13 @@ namespace embed
      * @tparam Clock The `embed::CClock` like clock that the scheduler should use. Defines the tick type, timer overflow, duration, time point and `now()` function.
      * @tparam n_tasks The maximum number of thats that will be pre-allocated for this scheduler.
      * @tparam FSleepUntil A function in the form `void sleep_until(Clock::time_point time)` that will be called if there is nothing to do and the MCU can enter sleep mode.
-     * @tparam log A pointer to an output stream. If assigned the Scheduler will print logs. Logs contain: Added task, resumed task, task moved to different queue.
+     * @tparam logger A logger that implements the functions defined by `embed::CRealTimeSchedulerLogger`
      * 
      */
-    template<CClock Clock, size_t n_tasks, void (*FSleepUntil)(typename Clock::time_point) = default_sleep_until<Clock>, ILogger<Clock>* logger = nullptr>
+    template<
+        CClock Clock, size_t n_tasks, 
+        void (*FSleepUntil)(typename Clock::time_point) = default_sleep_until<Clock>, 
+        CRealTimeSchedulerLogger logger = NullLogger<Clock>>
     class RealTimeScheduler {
     public:
         using clock = Clock;
@@ -118,7 +185,7 @@ namespace embed
         //       consider if the complexity is worth it - probably not!
         dual_priority_queue_type _priority_queue; // ready + deadline
         ArrayList<RTTask*, n_tasks> _await_bench;
-        unsigned int _next_task_id; // next id for the next added task
+        unsigned int _next_task_id = 0; // next id for the next added task
 
     private:
 
@@ -133,7 +200,7 @@ namespace embed
             // promote await back into running queue
             for(RTTask* task : this->_await_bench){
                 if(!task->is_awaiting()){
-                    if constexpr (logger != nullptr) logger->log_move(Clock::now(), task->name(), task->id(), "await", "run");
+                    logger::log_move(Clock::now(), task->name(), task->id(), "await", "run");
                     this->running_queue().push(task);
                 }
             }
@@ -145,7 +212,7 @@ namespace embed
                     RTTask* task = this->waiting_queue().top();
                     const time_point now = Clock::now();
                     if(task->ready_time() <= now){
-                        if constexpr (logger != nullptr) logger->log_move(now, task->name(), task->id(), "wait", "run");
+                        logger::log_move(now, task->name(), task->id(), "wait", "run");
                         this->waiting_queue().pop();
                         this->running_queue().push(task);
                     }else{
@@ -164,7 +231,7 @@ namespace embed
                 RTTask* task = this->running_queue().top_pop();
                 task->_execution_start = Clock::now();
                 task->resume();
-                if constexpr (logger != nullptr) logger->log_resume(task->_execution_start, Clock::now(), task->name(), task->id());
+                logger::log_resume(task->_execution_start, Clock::now(), task->name(), task->id());
                 // re-schedule
                 
                 const CoTaskSignal signal = task->get_signal();
@@ -172,24 +239,24 @@ namespace embed
                     case CoTaskSignal::Type::Await : {
                         // clear signal
                         this->_await_bench.emplace_back(task);
-                        if constexpr (logger != nullptr) logger->log_move(Clock::now(), task->name(), task->id(), "resume", "await");
+                        logger::log_move(Clock::now(), task->name(), task->id(), "resume", "await");
                     }break;
                     case CoTaskSignal::Type::NextCycle : {
                         task->_schedule = task->next_schedule(task->_schedule, ExecutionTime<Clock>{task->_execution_start, Clock::now()});
                         this->waiting_queue().push(task);
-                        if constexpr (logger != nullptr) logger->log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
+                        logger::log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
                     }break;
                     case CoTaskSignal::Type::ImplicitDelay : {
                         const duration rel_deadline = task->_schedule.deadline - task->_schedule.ready;
                         task->_schedule.ready = Clock::now() + std::chrono::duration_cast<duration>(signal.implicit_delay().delay);
                         task->_schedule.deadline = task->_schedule.ready + rel_deadline;
                         this->waiting_queue().push(task);
-                        if constexpr (logger != nullptr) logger->log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
+                        logger::log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
                     }break;
                     case CoTaskSignal::Type::ExplicitDelay : {
                         task->_schedule.ready = Clock::now() + std::chrono::duration_cast<duration>(signal.explicit_delay().delay);
                         task->_schedule.deadline = task->_schedule.ready + std::chrono::duration_cast<duration>(signal.explicit_delay().rel_deadline);
-                        if constexpr (logger != nullptr) logger->log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
+                        logger::log_move(Clock::now(), task->name(), task->id(), "resume", "wait");
                     }break;
                     case CoTaskSignal::Type::None : {
                         /* 
@@ -197,7 +264,7 @@ namespace embed
                         - task probably finished 
                         - let it die by not inserting it into lists
                         */
-                       if constexpr (logger != nullptr) logger->log_remove(Clock::now(), task->name(), task->id());
+                       logger::log_delete(Clock::now(), task->name(), task->id());
                     } break;
                     default : {
                         /* 
@@ -205,7 +272,7 @@ namespace embed
                         - task probably finished 
                         - let it die by not inserting it into lists
                         */
-                       if constexpr (logger != nullptr) logger->log_remove(Clock::now(), task->name(), task->id());
+                       logger::log_delete(Clock::now(), task->name(), task->id());
                     } break;
                 }
             }
@@ -218,10 +285,10 @@ namespace embed
 
             const time_point now = Clock::now();
             if(task->ready_time() <= now){
-                if constexpr (logger != nullptr) logger->log_add(now, task->name(), task->id(), "run");
+                logger::log_add(now, task->name(), task->id(), "run");
                 this->running_queue().push(task);
             }else{
-                if constexpr (logger != nullptr) logger->log_add(now, task->name(), task->id(), "wait");
+                logger::log_add(now, task->name(), task->id(), "wait");
                 this->waiting_queue().push(task);
             }
         }
@@ -232,23 +299,59 @@ namespace embed
         }
 
         /**
-         * @brief returns `true` if there are no tasks in its running queue and the scheduler is waiting for a task to get ready.
+         * @brief returns the number of tasks currently in the waiting queue
+         * 
+         * Tasks that are in the waiting queue are waiting for time to pass until `Clock::now()`
+         * is larger than the ready time of a task.
          */
-        bool is_waiting() const {
+        inline size_t n_waiting() const {
+            return this->waiting_queue().size();
+        }
+
+        /**
+         * @brief returns the number of tasks currently in the running queue
+         * 
+         * Tasks that are in the ready queue are all tasks that have a ready time that is larger
+         * than `Clock::now()`.
+         */
+        inline size_t n_running() const {
+            return this->running_queue().size();
+        }
+
+        /**
+         * @brief returns the number of tasks currently in the awaiting queue
+         * 
+         * Tasks that are in the awaiting queue are waiting for a future or awaitable to become ready.
+         */
+        inline size_t n_awaiting() const {
+            return this->_await_bench.size();
+        }
+
+        /**
+         * @brief returns `true` if there are no tasks in its running queue
+         */
+        inline bool is_waiting() const {
             return this->running_queue().empty();
         }
 
         /**
          * @brief returns `true` if there are tasks in its running queue and `false` if the scheduler is waiting.
          */
-        bool is_bussy() const {
+        inline bool is_bussy() const {
             return !this->running_queue().empty();
         }
 
         /**
          * @brief returns `true` if there are no tasks in any queue
          */
-        bool is_empty() const {
+        inline bool is_empty() const {
+            return this->_priority_queue.empty() && this->_await_bench.empty();
+        }
+
+        /**
+         * @brief returns `true` if there are no tasks in the sheduler
+         */
+        inline bool is_done() const {
             return this->_priority_queue.empty() && this->_await_bench.empty();
         }
     };
