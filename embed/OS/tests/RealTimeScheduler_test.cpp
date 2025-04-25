742be5feb259da13d17dce8d6aac2a703745f7fe
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-25 02:42:31 +0200

    added test

--- embed/OS/tests/RealTimeScheduler_test.cpp
+++ embed/OS/tests/RealTimeScheduler_test.cpp
@@ -4,19 +4,23 @@
 #include <embed/OS/Clock.hpp>
 #include <embed/OS/RealTimeTask.hpp>
 #include <embed/OS/RealTimeScheduler.hpp>
+#include <embed/Memory/Allocator.hpp>
 
 namespace embed
 {
     namespace
     {
-        
-        uint32_t _mock_time = 0;
-        uint32_t get_time(){return _mock_time;}
+        uint32_t g_mock_time = 0;
+        uint32_t get_time(){return g_mock_time;}
 
         TestResult one_task_immediatelly_ready_finishes_instantly(){
             TEST_START;
 
+            g_mock_time = 0;
             using MockClock = Clock<uint32_t, std::micro, get_time>;
+
+            embed::StaticLinearAllocatorDebug<1024> allocator;
+            embed::coroutine_frame_allocator = &allocator;
 
             class CoSuit{
                 public:
@@ -52,6 +56,60 @@ namespace embed
             TEST_END;
         }
 
+        TestResult one_task_delayed_ready_finishes_instantly(){
+            TEST_START;
+
+            // global state setup
+            g_mock_time = 0;
+            using MockClock = Clock<uint32_t, std::micro, get_time>;
+
+            embed::StaticLinearAllocatorDebug<1024> allocator;
+            embed::coroutine_frame_allocator = &allocator;
+
+            // task setup
+            class Task : public RealTimeTask<MockClock>{
+                public:
+                int proof = 0;
+
+                Task(const char* name, MockClock::duration ready, MockClock::duration deadline) 
+                    : RealTimeTask<MockClock>(this->main(), name, ready, deadline){}
+
+                Coroutine<Exit> main(){
+                    this->proof = 258;
+                    co_return Exit::Success;
+                }
+            };
+
+            Task simpleTask("simpleTask", 1us, 2us);
+
+            RealTimeScheduler<MockClock, 1, embed::default_sleep_until<MockClock>> scheduler;
+
+            // added task is not done
+            scheduler.add(&simpleTask);
+
+            TEST_TRUE(scheduler.is_waiting());
+            TEST_FALSE(simpleTask.is_done());
+            TEST_EQUAL(simpleTask.proof, 0);
+
+            // spinning does not help because the time is not right yet
+            scheduler.spin();
+
+            TEST_TRUE(scheduler.is_waiting());
+            TEST_FALSE(simpleTask.is_done());
+            TEST_EQUAL(simpleTask.proof, 0);
+
+            // only after time passes the task becomes ready and executed
+            ++g_mock_time;
+            scheduler.spin();
+
+            TEST_TRUE(scheduler.is_waiting());
+            TEST_TRUE(scheduler.is_empty());
+            TEST_TRUE(simpleTask.is_done());
+            TEST_EQUAL(simpleTask.proof, 258);
+
+            TEST_END;
+        }
+
     } // private namespace
     
 
@@ -59,7 +117,8 @@ namespace embed
         TEST_GROUP;
 
         return TestResult()
-            | one_task_immediatelly_ready_finishes_instantly;
+            | one_task_immediatelly_ready_finishes_instantly
+            | one_task_delayed_ready_finishes_instantly
             ;
     }
 
