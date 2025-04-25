e46b11d0fa689abcb4505ca2a72a1152e00a7647
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-25 21:31:33 +0200

    + test that tests two tasks with different ready and deadline priorities

--- embed/OS/tests/RealTimeScheduler_test.cpp
+++ embed/OS/tests/RealTimeScheduler_test.cpp
@@ -5,6 +5,7 @@
 #include <embed/OS/RealTimeTask.hpp>
 #include <embed/OS/RealTimeScheduler.hpp>
 #include <embed/Memory/Allocator.hpp>
+#include <embed/OS/Delay.hpp>
 
 namespace embed
 {
@@ -110,7 +111,224 @@ namespace embed
             TEST_END;
         }
 
+    TestResult one_task_immediatelly_ready_delays(){
+        TEST_START;
+
+        // global state setup
+        g_mock_time = 0;
+        using MockClock = Clock<uint32_t, std::micro, get_time>;
+
+        embed::StaticLinearAllocatorDebug<1024> allocator;
+        embed::coroutine_frame_allocator = &allocator;
+
+        // task setup
+        class Task : public RealTimeTask<MockClock>{
+            public:
+            int proof = 0;
+
+            Task(const char* name, MockClock::duration ready, MockClock::duration deadline) 
+                : RealTimeTask<MockClock>(this->main(), name, ready, deadline){}
+
+            Coroutine<Exit> main(){
+                this->proof = 1;
+                co_await Delay(2us);
+                this->proof = 2;
+                co_return Exit::Success;
+            }
+        };
+
+        Task task("Task", 1us, 2us);
+
+        // OutputLogger<MockClock>::stream = embed::cout;
+        // RealTimeScheduler<MockClock, 1, embed::default_sleep_until<MockClock>, OutputLogger<MockClock>> scheduler;
+
+        RealTimeScheduler<MockClock, 1> scheduler;
+
+        // added task is not done
+        scheduler.add(&task);
+
+        TEST_EQUAL(scheduler.n_waiting(), 1);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_EQUAL(task.proof, 0);
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task.is_done());
+
+        g_mock_time = 0;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 1);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_EQUAL(task.proof, 0);
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task.is_done());
+
+        g_mock_time = 1;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 1);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_EQUAL(task.proof, 1);
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task.is_done());
+
+        g_mock_time = 2;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 1);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_EQUAL(task.proof, 1);
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task.is_done());
+
+        g_mock_time = 3;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 0);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_EQUAL(task.proof, 2);
+        TEST_TRUE(scheduler.is_done());
+        TEST_TRUE(task.is_done());
+
+        TEST_END;
+    }
+
+
+    TestResult two_tasks_first_has_lower_ready_second_has_lower_deadline(){
+        TEST_START;
+
+        // global state setup
+        g_mock_time = 0;
+        using MockClock = Clock<uint32_t, std::micro, get_time>;
+
+        embed::StaticLinearAllocatorDebug<1024> allocator;
+        embed::coroutine_frame_allocator = &allocator;
+
+        // task setup
+        class Task : public RealTimeTask<MockClock>{
+            /*
+            Tests the Earliest-Ready-Time / Earliest-Deadline scheduling priorities
+             */
+            public:
+            int proof = 0;
+
+            Task(const char* name, MockClock::duration ready, MockClock::duration deadline) 
+                : RealTimeTask<MockClock>(this->main(), name, ready, deadline){}
+
+            Coroutine<Exit> main(){
+                this->proof = 1;
+                co_await Delay(0us); // basically yield to other tasks
+                this->proof = 2;
+                co_return Exit::Success;
+            }
+        };
+
+        Task task1("Task1", 1us, 4us);
+        Task task2("Task2", 2us, 2us);
+
+        // OutputLogger<MockClock>::stream = embed::cout;
+        // RealTimeScheduler<MockClock, 2, embed::default_sleep_until<MockClock>, OutputLogger<MockClock>> scheduler;
+        RealTimeScheduler<MockClock, 2> scheduler;
+
+        // added task is not done
+        scheduler.add(&task1);
+
+        scheduler.add(&task2);
+
+        TEST_EQUAL(scheduler.n_waiting(), 2);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_TRUE(scheduler.is_waiting());
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(scheduler.is_busy());
+        TEST_TRUE(scheduler.is_full());
+        TEST_NOT_EQUAL(task1.id(), task2.id())
+        TEST_FALSE(task1.is_done());
+        TEST_EQUAL(task1.proof, 0);
+        TEST_FALSE(task2.is_done());
+        TEST_EQUAL(task2.proof, 0);
+
+        g_mock_time = 0;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 2);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_TRUE(scheduler.is_waiting());
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(scheduler.is_busy());
+        TEST_TRUE(scheduler.is_full());
+        TEST_NOT_EQUAL(task1.id(), task2.id()); // unique id's
+        TEST_FALSE(task1.is_done());
+        TEST_EQUAL(task1.proof, 0);
+        TEST_FALSE(task2.is_done());
+        TEST_EQUAL(task2.proof, 0);
+        
+        g_mock_time = 1;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 2);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_TRUE(scheduler.is_waiting());
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(scheduler.is_busy());
+        TEST_FALSE(task1.is_done());
+        TEST_EQUAL(task1.proof, 1);
+        TEST_FALSE(task2.is_done());
+        TEST_EQUAL(task2.proof, 0);
+
+        g_mock_time = 2;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 1);
+        TEST_EQUAL(scheduler.n_running(), 1);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_FALSE(scheduler.is_waiting());
+        TEST_TRUE(scheduler.is_busy());
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task1.is_done());
+        TEST_EQUAL(task1.proof, 1);
+        TEST_FALSE(task2.is_done());
+        TEST_EQUAL(task2.proof, 1);
+
+        g_mock_time = 3;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 0);
+        TEST_EQUAL(scheduler.n_running(), 1);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_FALSE(scheduler.is_waiting());
+        TEST_TRUE(scheduler.is_busy());
+        TEST_FALSE(scheduler.is_done());
+        TEST_FALSE(task1.is_done());
+        TEST_EQUAL(task1.proof, 1);
+        TEST_TRUE(task2.is_done());
+        TEST_EQUAL(task2.proof, 2);
+
+        g_mock_time = 4;
+        scheduler.spin();
+
+        TEST_EQUAL(scheduler.n_waiting(), 0);
+        TEST_EQUAL(scheduler.n_running(), 0);
+        TEST_EQUAL(scheduler.n_awaiting(), 0);
+        TEST_TRUE(scheduler.is_waiting());
+        TEST_FALSE(scheduler.is_busy());
+        TEST_TRUE(scheduler.is_done());
+        TEST_TRUE(task1.is_done());
+        TEST_EQUAL(task1.proof, 2);
+        TEST_TRUE(task2.is_done());
+        TEST_EQUAL(task2.proof, 2);
+
+        TEST_END;
+    }
+
     } // private namespace
+
     
 
     TestResult RealTimeScheduler_test(){
@@ -119,6 +337,8 @@ namespace embed
         return TestResult()
             | one_task_immediatelly_ready_finishes_instantly
             | one_task_delayed_ready_finishes_instantly
+            | one_task_immediatelly_ready_delays
+            | two_tasks_first_has_lower_ready_second_has_lower_deadline
             ;
     }
 
