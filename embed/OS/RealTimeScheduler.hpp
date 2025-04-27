a4c3dbdd4525228f82168fbcd97ce5ad05b5ec9c
Tobias Wallner <tobias.wallner1@gmx.net>
2025-04-27 21:50:17 +0200

    added printing of the scheduler internals displaying the tasks, their current queue and their timeing constraints

--- embed/OS/RealTimeScheduler.hpp
+++ embed/OS/RealTimeScheduler.hpp
@@ -2,12 +2,18 @@
 
 // std
 #include <variant>
+#include <concepts>
+#include <ranges>
+#include <string_view>
 
 // embed
 #include <embed/OS/Clock.hpp>
 #include <embed/Containers/ArrayList.hpp>
 #include <embed/Containers/DualPriorityQueue.hpp>
 #include <embed/OS/RealTimeTask.hpp>
+#include <embed/OStream/OStream.hpp>
+#include <embed/OStream/ansi.hpp>
+#include <embed/OStream/utf8_lines.hpp>
 
 namespace embed
 {
@@ -20,15 +26,18 @@ namespace embed
         return;
     }
 
+    /**
+     * @brief The concept for a logging device that can be passed into the RealTimeScheduler
+     */
     template<class Logger>
     concept CRealTimeSchedulerLogger = requires(
         Logger::time_point time, 
         Logger::time_point time_after, 
         Logger::time_point time_until, 
-        const char* name, 
+        std::string_view name, 
         unsigned int id, 
-        const char* from_queue, 
-        const char* to_queue) 
+        std::string_view from_queue, 
+        std::string_view to_queue) 
     {
         typename Logger::time_point;
 
@@ -39,6 +48,9 @@ namespace embed
         { Logger::log_sleep(time, time_until) };
     };
 
+    /**
+     * @brief A default logger that does nothing
+     */
     template<CClock Clock>
     class NullLogger {
     public:
@@ -46,14 +58,27 @@ namespace embed
         using time_point = typename Clock::time_point;
         using duration = typename Clock::duration;
 
-        static inline void log_add([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id, [[maybe_unused]]const char* to_queue){}
-        static inline void log_move([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id, [[maybe_unused]]const char* from_queue, [[maybe_unused]]const char* to_queue){}
-        static inline void log_resume([[maybe_unused]]time_point time_from, [[maybe_unused]]time_point time_after, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id){}
-        static inline void log_delete([[maybe_unused]]time_point time, [[maybe_unused]]const char* name, [[maybe_unused]]unsigned int id){}
+        static inline void log_add([[maybe_unused]]time_point time, [[maybe_unused]]std::string_view name, [[maybe_unused]]unsigned int id, [[maybe_unused]]std::string_view to_queue){}
+        static inline void log_move([[maybe_unused]]time_point time, [[maybe_unused]]std::string_view name, [[maybe_unused]]unsigned int id, [[maybe_unused]]std::string_view from_queue, [[maybe_unused]]std::string_view to_queue){}
+        static inline void log_resume([[maybe_unused]]time_point time_from, [[maybe_unused]]time_point time_after, [[maybe_unused]]std::string_view name, [[maybe_unused]]unsigned int id){}
+        static inline void log_delete([[maybe_unused]]time_point time, [[maybe_unused]]std::string_view name, [[maybe_unused]]unsigned int id){}
         static inline void log_sleep([[maybe_unused]]time_point time, [[maybe_unused]]time_point sleep_until){}
 
     };
 
+    /**
+     * @brief A logger that will write all loggings directly to an output stream.
+     * 
+     * To set its output stream write to:
+     * ```
+     * OutputLogger<MyClock>::stream = myStream;
+     * ```
+     * 
+     * @tparam Clock A Clock type that conforms to the `embed::CClock` concept.
+     * 
+     * @see embed::CClock
+     * @see embed::RealTimeScheduler
+     */
     template<CClock Clock>
     class OutputLogger {
     public:
@@ -63,7 +88,7 @@ namespace embed
 
         static inline OStreamRef stream;
 
-        static void print_symbol(const char* symbol){
+        static void print_symbol(std::string_view symbol){
             stream << embed::ansi::blue << symbol;
         }
 
@@ -75,15 +100,15 @@ namespace embed
             stream << embed::ansi::light_blue << duration;
         }
 
-        static void print_primary_action(const char* action){
+        static void print_primary_action(std::string_view action){
             stream << " " << embed::ansi::light_orange << action << " ";
         }
 
-        static void print_secondary_action(const char* action){
+        static void print_secondary_action(std::string_view action){
             stream << " " << embed::ansi::blue << action << " ";
         }
 
-        static void print_name(const char* name){
+        static void print_name(std::string_view name){
             stream << embed::ansi::light_blue << name;
         }
 
@@ -92,57 +117,64 @@ namespace embed
         }
 
         static void print_time(time_point time){
-            print_symbol("@");
+            using namespace std::string_view_literals;
+            print_symbol("@"sv);
             print_time_point(time);
         }
 
-        static void print_task(const char* name, unsigned int id){
-            print_symbol("{");
+        static void print_task(std::string_view name, unsigned int id){
+            using namespace std::string_view_literals;
+            print_symbol("{"sv);
             print_name(name);
-            print_symbol(", ");
+            print_symbol(", "sv);
             print_id(id);
-            print_symbol("}");
+            print_symbol("}"sv);
         }
 
-        static void log_add(time_point time, const char* name, unsigned int id, const char* to_queue){
+        static void log_add(time_point time, std::string_view name, unsigned int id, std::string_view to_queue){
+            using namespace std::string_view_literals;
             print_time(time);
-            print_primary_action("add");
+            print_primary_action("add"sv);
             print_task(name, id);
-            print_secondary_action("to");
+            print_secondary_action("to"sv);
             print_name(to_queue);
             stream << embed::ansi::reset << embed::endl;
         }
 
-        static void log_move(time_point time, const char* name, unsigned int id, const char* from_queue, const char* to_queue){
+        static void log_move(time_point time, std::string_view name, unsigned int id, std::string_view from_queue, std::string_view to_queue){
+            using namespace std::string_view_literals;
             print_time(time);
-            print_primary_action("move");
+            print_primary_action("move"sv);
             print_task(name, id);
-            print_secondary_action("from");
+            print_secondary_action("from"sv);
             print_name(from_queue);
-            print_secondary_action("to");
+            print_secondary_action("to"sv);
             print_name(to_queue);
             stream << embed::ansi::reset << embed::endl;
         }
 
-        static void log_resume(time_point time_from, time_point time_after, const char* name, unsigned int id){
+        static void log_resume(time_point time_from, time_point time_after, std::string_view name, unsigned int id){
+            using namespace std::string_view_literals;
             print_time(time_from);
-            print_primary_action("resume");
+            print_primary_action("resume"sv);
             print_task(name, id);
-            print_secondary_action("time");
+            print_secondary_action("time"sv);
             print_time_point(time_after - time_from);
             stream << embed::ansi::reset << embed::endl;
         }
 
-        static void log_delete(time_point time, const char* name, unsigned int id){
+        static void log_delete(time_point time, std::string_view name, unsigned int id){
+            using namespace std::string_view_literals;
             print_time(time);
-            print_primary_action("delete");
+            print_primary_action("delete"sv);
             print_task(name, id);
             stream << embed::ansi::reset << embed::endl;
         }
 
         static void log_sleep(time_point time, time_point sleep_until){
+            using namespace std::string_view_literals;
             print_time(time);
-            print_primary_action("sleep until");
+            print_primary_action("sleep until"sv);
             print_time_point(sleep_until);
             stream << embed::ansi::reset << embed::endl;
         }
@@ -171,7 +203,7 @@ namespace embed
         using clock = Clock;
         using time_point = typename Clock::time_point;
         using duration = typename Clock::duration;
-
+        
     private:
         using RTTask = RealTimeTask<Clock>;
         using dual_priority_queue_type = DualPriorityQueue<RTTask*, n_tasks, larger_ready_time<Clock>, larger_deadline<Clock>>;
@@ -199,6 +231,12 @@ namespace embed
         waiting_queue_const_ref waiting_queue() const {return this->_priority_queue;}
         running_queue_const_ref running_queue() const {return this->_priority_queue;}
 
+        /**
+         * @brief Moves tasks that got ready from the waiting- and awaiting-queue into the running queue
+         * 
+         * 1. Checks all tasks from the awaiting-queue that return `true` on `.await_ready()` and moves them into the running queue.
+         * 2. Moves the top of the waiting priority queue that got ready into the running queue.
+         */
         void promote(){
             // promote await back into running queue
             for(RTTask* task : this->_await_bench){
@@ -223,6 +261,20 @@ namespace embed
             }
         }
 
+        /**
+         * @brief Runs the next task and decides to which queue it belongs after running.
+         * 
+         * Runs the task with the highest priority (= lowest deadline) from the running queue. 
+         * The task may send a signal using `embed::CoTaskSignal` from an `embed::AwaitableNode`.
+         * If the task sent a: 
+         * - <b><code>NextCycle</code> signal</b>: the scheduler will call the tasks `.next_schedule()` method to calculate its next schedule and puts it back into the waiting priority list.
+         * - <b><code>Await</code> signal</b>: the task will be put on the `await queue` until the awaitable signals `true` on `.await_ready()`.
+         * - <b><code>Implicit/ExplicitDelay</code> signal</b>: the scheduler will calculate the next schedule of the task using the given delay.
+         * - <b><code>None</code></b>: (happens when the task ends) the task will be removed from the scheduler and not put back into any queue.
+         * 
+         * @see embed::CoTaskSignal
+         * @see embed::AwaitableNode
+         */
         void run_next(){
             if(this->running_queue().empty()){
                 FSleepUntil(this->waiting_queue().top()->ready_time());
@@ -395,31 +447,144 @@ namespace embed
             return this->_priority_queue.empty() && this->_await_bench.empty();
         }
 
+    private:
 
-        friend OStream& operator<<(OStream& stream, const RealTimeScheduler& scheduler){
+        /**
+         * @brief prints a scheduler queue to the stream
+         * @param stream An `embed::OStream` reference
+         * @param taskList A range that contains `RTTask*` objects
+         */
+        template<std::ranges::range TaskList>
+        static void print_task_list(OStream& stream, const TaskList& taskList, const int indentation = 0){
+            using namespace std::string_view_literals;
+            using namespace embed::utf8_lines;
+
+            // get length of longest name
+            int max_name_length = 4;
+            for(const RTTask* task : taskList){
+                const int name_size = static_cast<int>(task->name().size());
+                max_name_length =  (name_size > max_name_length) ? name_size : max_name_length;
+            }
+
+            const int id_width = 12;
+            const int time_width = 24;
+
+            // table top
+            stream.put(' ', indentation);
+            stream << single_corner_topleft << single_horizontal;
+            for(int i = 0; i < max_name_length; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_up << single_horizontal;
+            for(int i = 0; i < id_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_up << single_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_up << single_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_corner_topright;
+            stream << embed::newl;
+
+            // print header
+            stream.put(' ', indentation);
+            stream << single_vertical << ' ';
+            stream << FormatStr("name"sv).mwidth(max_name_length).left();
+            stream << ' ' << single_vertical << ' ';
+            stream << FormatStr("id"sv).mwidth(id_width).right();
+            stream << ' ' << single_vertical << ' ';
+            stream << FormatStr("ready"sv).mwidth(time_width).right();
+            stream << ' ' << single_vertical << ' ';
+            stream << FormatStr("deadline"sv).mwidth(time_width).right();
+            stream << ' ' << single_vertical << embed::newl;
+
+            // header underline
+            stream.put(' ', indentation);
+            stream << mixed_t_left << double_horizontal;
+            for(int i = 0; i < max_name_length; ++i) stream << double_horizontal;
+            stream << double_horizontal << mixed_cross << double_horizontal;
+            for(int i = 0; i < id_width; ++i) stream << double_horizontal;
+            stream << double_horizontal << mixed_cross << double_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << double_horizontal;
+            stream << double_horizontal << mixed_cross << double_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << double_horizontal;
+            stream << double_horizontal << mixed_t_right;
+            stream << embed::newl;
+
+            // print table
+            for(const RTTask* task : taskList){
+                stream.put(' ', indentation);
+                stream << single_vertical << ' ';
+                stream << FormatStr(task->name()).mwidth(max_name_length).left();
+                stream << ' ' << single_vertical << ' ';
+                stream << FormatInt(task->id()).mwidth(id_width).right();
+                stream << ' ' << single_vertical << ' ';
+                stream << format_chrono(task->ready_time().time_since_epoch()).mwidth(time_width).right();
+                stream << ' ' << single_vertical << ' ';
+                stream << format_chrono(task->deadline().time_since_epoch()).mwidth(time_width).right();
+                stream << ' ' << single_vertical << embed::newl;
+            }
+
+            // table bottom
+            stream.put(' ', indentation);
+            stream << single_corner_botleft << single_horizontal;
+            for(int i = 0; i < max_name_length; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_down << single_horizontal;
+            for(int i = 0; i < id_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_down << single_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_t_down << single_horizontal;
+            for(int i = 0; i < time_width; ++i) stream << single_horizontal;
+            stream << single_horizontal << single_corner_botright;
+            stream << embed::newl;
+        }
+    public:
+
+        /**
+         * @brief prints the current state of the scheduler. Lists all queues and contained tasks.
+         * 
+         * Example output:
+         * ```
+         * @2us Running:
+         *   ┌────────┬──────────────┬──────────────────────────┬──────────────────────────┐
+         *   │ name   │           id │                    ready │                 deadline │
+         *   ╞════════╪══════════════╪══════════════════════════╪══════════════════════════╡
+         *   │ Task 1 │            0 │                      1us │                      5us │
+         *   └────────┴──────────────┴──────────────────────────┴──────────────────────────┘
+         *
+         * @2us Waiting:
+         *   ┌──────────┬──────────────┬──────────────────────────┬──────────────────────────┐
+         *   │ name     │           id │                    ready │                 deadline │
+         *   ╞══════════╪══════════════╪══════════════════════════╪══════════════════════════╡
+         *   │ Task two │            1 │                      2us │                      4us │
+         *   └──────────┴──────────────┴──────────────────────────┴──────────────────────────┘
+         *
+         * @2us Awaiting:
+         *   ┌──────┬──────────────┬──────────────────────────┬──────────────────────────┐
+         *   │ name │           id │                    ready │                 deadline │
+         *   ╞══════╪══════════════╪══════════════════════════╪══════════════════════════╡
+         *   └──────┴──────────────┴──────────────────────────┴──────────────────────────┘
+         * ```
+         * 
+         * @param stream A reference to an `embed::OStream` object
+         */
+        void print(OStream& stream) const {
             const time_point now = Clock::now();
             stream << "@" << now << " Running: " << embed::newl;
-            stream << "name | id | ready time | deadline" << embed::newl;
-            stream << "------------------------------------------" << embed::newl;
-            for(const RTTask* task : scheduler.running_queue()){
-                stream << task->name() << " | " << task->id() << " | " << task->ready_time() << " | " << task->deadline() << embed::newl;
-            }
+            RealTimeScheduler::print_task_list(stream, this->running_queue(), 2);
+            stream << embed::newl << "@" << now << " Waiting: " << embed::newl;
+            RealTimeScheduler::print_task_list(stream, this->waiting_queue(), 2);
+            stream << embed::newl << "@" << now << " Awaiting: " << embed::newl;
+            RealTimeScheduler::print_task_list(stream, this->_await_bench, 2);
             stream << embed::newl;
-            stream << "@" << now << " Waiting: " << embed::newl;
-            stream << "name | id | ready time | deadline" << embed::newl;
-            stream << "------------------------------------------" << embed::newl;
-            for(const RTTask* task : scheduler.waiting_queue()){
-                stream << task->name() << " | " << task->id() << " | " << task->ready_time() << " | " << task->deadline() << embed::newl;
-            }
-            stream << embed::newl;
-            stream << "@" << now << " Awaiting: " << embed::newl;
-            stream << "name | id | ready time | deadline" << embed::newl;
-            stream << "------------------------------------------" << embed::newl;
-            for(const RTTask* task : scheduler._await_bench){
-                stream << task->name() << " | " << task->id() << " | " << task->ready_time() << " | " << task->deadline() << embed::newl;
-            }
+        }
+        
+        /**
+         * @brief prints the state of the scheduler. Lists all queues and their contained tasks.
+         */
+        friend OStream& operator<<(OStream& stream, const RealTimeScheduler& scheduler){
+            scheduler.print(stream);
             return stream;
         }
+
     };
     
+    
+
 } // namespace embed
